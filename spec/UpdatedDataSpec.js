a88e62e083f7ac7d93a7dbbc1a82ee0afc9be73a
Doug Franklin <doug@dougfranklin.com>
2021-02-20 10:29:58 -0500

    All updates logged

--- spec/UpdatedDataSpec.js
+++ spec/UpdatedDataSpec.js
@@ -4,23 +4,21 @@ describe('Updated Data Spec', () => {
 
     let allPromises = Promise.all([tryUrl('/users'), tryUrl('/hobbies'), tryUrl('/favorites')])
       .then(result => {
-        // console.log('it-then: result', result);
-        // expect(result).toEqual(result);
         expect(true).toEqual(true);        
         return result;
       })
       describe('CorrectedDataSpec: allPromises, working with the Promise.all()', () => {
-        console.log('allPromises', allPromises);
+        // console.log('allPromises', allPromises);
         
         it ('CorrectedDataSpec: Promise.all() Elements are available here', (done) => {
           allPromises.then((elements) => {
-            console.log('elements', elements);
+            // console.log('elements', elements);
 
             let usrs = elements[0];
             let hobs = elements[1];
             let favs = elements[2];
-
-            console.log('usrs', usrs, 'hobs', hobs, 'favs', favs);
+            
+            // console.log('usrs', usrs, 'hobs', hobs, 'favs', favs);
 
             expect(true).toBe(true);
             done();
@@ -36,7 +34,38 @@ describe('Updated Data Spec', () => {
             elements[1] = correctHobbies(elements[1]);
             elements[2] = correctFavorites(elements[2]);
 
-            console.log('elements: after', elements);
+            // console.log('elements: after', elements);
+
+            expect(true).toBe(true);
+            done();
+          })
+        }) ///////////////////////////////////
+
+        it ('CorrectedDataSpec: Update the elements', (done) => {
+          allPromises.then(elements => {
+
+            // console.log('elements: before', elements); // same as line 40
+
+            elements[0] = tryUrl('/updateUsers', elements[0], 'post')
+                .then((updatedUsers) => {
+                    console.log('updatedUser', updatedUsers);
+                })
+
+            elements[1] = tryUrl('/updateHobbies', elements[1], 'post')
+                .then((updatedHobbies) => {
+                    console.log('updatedHobbies', updatedHobbies);
+                })
+                
+            elements[2] = tryUrl('/updateFavorites', elements[2], 'post')
+                .then((updatedFavorites) => {
+                    console.log('updatedFavorites', updatedFavorites);
+                })
+
+            // elements[0] = correctUsers(elements[0], 'PA');
+            // elements[1] = correctHobbies(elements[1]);
+            // elements[2] = correctFavorites(elements[2]);
+
+            console.log('updated elements: after', elements);
 
             expect(true).toBe(true);
             done();
