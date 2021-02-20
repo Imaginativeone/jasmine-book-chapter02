b457c3c35b81d3189cb693758c05d1ce9bcd8cce
Doug Franklin <doug@dougfranklin.com>
2021-02-20 09:38:35 -0500

    Cleanup

--- src/correctUsers.js
+++ src/correctUsers.js
@@ -1,12 +1,8 @@
 function correctUsers(users, state) {
-
   return users.filter(user => {
     if (!user.state) {
       user.state = state;
       return user;
     }
   })
-
-  // return users;
-  
 }
