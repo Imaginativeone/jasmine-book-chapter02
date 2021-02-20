860452ad33d3145f0b1c769f71fae62980a7230b
Doug Franklin <doug@dougfranklin.com>
2021-02-20 09:55:40 -0500

    Prep for UpdatedDataSpec.js

---
+++ spec/UpdatedDataSpec.js
@@ -0,0 +1,59 @@
+describe('Updated Data Spec', () => {
+
+  describe('The promise happens in here', () => {
+
+    let allPromises = Promise.all([tryUrl('/users'), tryUrl('/hobbies'), tryUrl('/favorites')])
+      .then(result => {
+        // console.log('it-then: result', result);
+        // expect(result).toEqual(result);
+        expect(true).toEqual(true);        
+        return result;
+      })
+      describe('CorrectedDataSpec: allPromises, working with the Promise.all()', () => {
+        console.log('allPromises', allPromises);
+        
+        it ('CorrectedDataSpec: Promise.all() Elements are available here', (done) => {
+          allPromises.then((elements) => {
+            console.log('elements', elements);
+
+            let usrs = elements[0];
+            let hobs = elements[1];
+            let favs = elements[2];
+
+            console.log('usrs', usrs, 'hobs', hobs, 'favs', favs);
+
+            expect(true).toBe(true);
+            done();
+          })
+        }) ///////////////////////////////////
+
+        it ('CorrectedDataSpec: Correct the elements', (done) => {
+          allPromises.then((elements) => {
+
+            // console.log('elements: before', elements); // same as line 40
+
+            elements[0] = correctUsers(elements[0], 'PA');
+            elements[1] = correctHobbies(elements[1]);
+            elements[2] = correctFavorites(elements[2]);
+
+            console.log('elements: after', elements);
+
+            expect(true).toBe(true);
+            done();
+          })
+        }) ///////////////////////////////////
+
+      })
+
+      it ('Another it', () => {
+        expect(true).toBe(true);
+      })
+  })
+
+  describe('Template', () => {})
+
+});
+
+// xdescribe('A', () => {
+//   it ('A', () => {})
+// });
